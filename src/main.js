65943f1f5a530d38e8b4eb1b8318be39e5aa8033
huyangyang <13621099447@163.com>
2018-11-15 19:01:59 +0800

    add tree

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import VCharts from 'v-charts'
 import './registerServiceWorker'
 import './plugins/axios'
 
-Vue.use(VCharts)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="home">
-        <ve-tree :data="chartData"></ve-tree>
+    <div ref="my-e" style="width: 100%;height:400px;">
     </div>
 </template>
 
@@ -9,26 +8,64 @@
         name: 'home',
         data() {
             return {
-                chartData: {
-                    columns: ['name', 'value'],
-                    rows: [
+                treeData: {}
+            }
+        },
+        methods: {
+            initE() {
+                let myChart = echarts.init(this.$refs['my-e'])
+                let option = {
+                    tooltip: {
+                        trigger: 'item',
+                        triggerOn: 'mousemove'
+                    },
+                    series: [
                         {
-                            name: 'tree1',
-                            value: []
+                            type: 'tree',
+                            data: [this.treeData],
+                            left: '2%',
+                            right: '2%',
+                            top: '8%',
+                            bottom: '20%',
+                            symbol: 'emptyCircle',
+                            expandAndCollapse: true,
+
+                            label: {
+                                normal: {
+                                    position: 'top',
+                                    rotate: -90,
+                                    verticalAlign: 'middle',
+                                    align: 'right',
+                                    fontSize: 9
+                                }
+                            },
+
+                            leaves: {
+                                label: {
+                                    normal: {
+                                        position: 'bottom',
+                                        rotate: -90,
+                                        verticalAlign: 'middle',
+                                        align: 'left'
+                                    }
+                                }
+                            },
+
+                            animationDurationUpdate: 750
                         }
                     ]
                 }
+                myChart.setOption(option)
             }
         },
-        components: {},
-        created() {
-            this.chartData.rows[0].value[0] = {
+        mounted() {
+            this.treeData = {
                 name: 'f',
                 value: 1,
                 link: 'https://ele.me',
                 children: [
                     {
-                        name: 'abc',
+                        name: 'a',
                         value: 1,
                         link: 'https://ele.me',
                         children: [
@@ -45,7 +82,7 @@
                         ]
                     },
                     {
-                        name: 'csw',
+                        name: 'b',
                         value: 1,
                         link: 'https://ele.me',
                         children: [
@@ -60,9 +97,44 @@
                                 value: 2
                             }
                         ]
+                    },
+                    {
+                        name: 'c',
+                        value: 3,
+                        link: 'https://ele.me',
+                        children: [
+                            {
+                                name: 'c-a',
+                                link: 'https://ele.me',
+                                value: 4
+                            },
+                            {
+                                name: 'c-b',
+                                link: 'https://ele.me',
+                                value: 2
+                            }
+                        ]
+                    },
+                    {
+                        name: 'd',
+                        value: 3,
+                        link: 'https://ele.me',
+                        children: [
+                            {
+                                name: 'd-a',
+                                link: 'https://ele.me',
+                                value: 4
+                            },
+                            {
+                                name: 'd-b',
+                                link: 'https://ele.me',
+                                value: 2
+                            }
+                        ]
                     }
                 ]
             }
+            this.initE()
         }
     }
 </script>
